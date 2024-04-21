aab54be823a1a6bacf6208c832c2c30cd4ccc9e1
unknown <montiste@tktk.ee>
2024-04-21 17:01:40 +0300

    first commit

---
+++ Arrays/Program.cs
@@ -0,0 +1,24 @@
+﻿//string[] fruit = { "apples", "bananas", "pears" };
+
+
+string[] fruit = new string[3];
+
+fruit[0] = "apples";
+fruit[1] = "bananas";
+fruit[2] = "pears";
+
+//Console.WriteLine($"{fruit[0]}, {fruit[1]}, {fruit[2]}");
+
+int fruitArrayLenght = fruit.Length;
+
+Console.WriteLine($"There are {fruitArrayLenght} fruits in your array.");
+
+for (int i = 0; i < fruitArrayLenght; i++)
+{
+    fruit[i] = fruit[i].Replace(fruit[i][0], char.ToUpper(fruit[i][0]));
+}
+
+foreach (string element in fruit)
+{
+    Console.WriteLine(element);
+}
---
+++ RandomElement/Program.cs
@@ -0,0 +1,6 @@
+﻿string[] snacks = { "chips", "yoghurt", "chocolates", "infants", "human souls" };
+Random random = new Random();
+
+int randomIndex = random.Next(0, snacks.Length);
+
+Console.WriteLine($"Tonight we are going to eat {snacks[randomIndex]}.");
